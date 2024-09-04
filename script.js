11c3301fd974ffeb02863a81cba621e6648646ee
Raslenarbi <raslen2007@gmail.com>
2024-09-04 14:22:19 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+/*function lessthan100 (a,b){
+    return (a+b) < 100;
+
+}
+console.log(lessThan100(40,50));
+
+function nameString(name) {
+    let result = name + "Edabit";
+    return result;
+}
+
+console.log(nameString("Mubashir"));  
+console.log(nameString("Matt"));      
+console.log(nameString("javaScript"));
+
+
+function giveMeSomething(a) {
+	return "something" + a;
+}
+console.log(giveMeSomething(" is cooking"))
+
+
+
+function joinWithSomething(a) {
+    return `"something ${a}"`;
+}
+
+// Example usage:
+console.log(joinWithSomething("nice"));  // Output: "something nice"
+console.log(joinWithSomething("cool"));  // Output: "something cool"
+
+
+
+var x= document.querySelector('h3');
+console.log(x);*/
+
+
+var x=document.getElementById("des")
+console.log(x);
+x.setAttribute("class", "btn")
+console.log(x);
+x.removeAttribute("class")
+console.log(x);
+
